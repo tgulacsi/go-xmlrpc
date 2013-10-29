252e644aa1d7cc6582476182488b97f8cf319fb7
Tamás Gulácsi <gt-waterhouse@gthomas.eu>
2013-10-29 21:50:13 +0100

    proper ServeHTTP

--- server.go
+++ server.go
@@ -107,6 +107,14 @@ func ServeConn(conn io.ReadWriteCloser) {
 // ServeConn uses the gob wire format (see package gob) on the
 // connection.  To use an alternate codec, use ServeCodec.
 func (server *XMLRpcServer) ServeConn(conn io.ReadWriteCloser) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("ServeCodec failed: %s", err)
+			io.WriteString(conn, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
+			Fault{Code: -1, Message: fmt.Sprintf("%s", err)}.WriteXML(conn)
+			conn.Close()
+		}
+	}()
 	// buf := bufio.NewWriter(conn)
 	srv := &serverCodec{conn: conn}
 	server.ServeCodec(srv)
@@ -117,17 +125,33 @@ type readWriteCloser struct {
 	io.Closer
 }
 
+type readWriter struct {
+	io.Reader
+	io.Writer
+}
+
+func (rw readWriter) Close() error {
+	return nil
+}
+
 // ServeHTTP implements an http.Handler that answers RPC req
 func (server *XMLRpcServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	log.Printf("connection from %s", req.RemoteAddr)
-	conn, buf, err := w.(http.Hijacker).Hijack()
-	if err != nil {
-		log.Print("rpc hijacking ", req.RemoteAddr, ": ", err.Error())
+	if req.Method == "CONNECT" { // Go RPC
+		conn, buf, err := w.(http.Hijacker).Hijack()
+		if err != nil {
+			log.Print("rpc hijacking ", req.RemoteAddr, ": ", err.Error())
+			return
+		}
+		io.WriteString(conn, "HTTP/1.0 200 Connected go Go XML-RPC server\r\n\r\n")
+		io.WriteString(conn, "Content-Type: text/xml\r\n")
+		server.ServeConn(readWriteCloser{buf, conn})
 		return
 	}
-	io.WriteString(conn, "HTTP/1.0 200 Connected go Go XML-RPC server\r\n\r\n")
-	io.WriteString(conn, "Content-Type: text/xml\r\n")
-	server.ServeConn(readWriteCloser{buf, conn})
+	// HTTP
+	defer req.Body.Close()
+	server.ServeConn(readWriter{req.Body, w})
+	return
 }
 
 // DefaultServer is the default server
